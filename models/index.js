68ee00d66a4ae93d5a08b51fa5bee47e78a6aa6b
Anne Saber <iarabejo3@gmail.com>
2022-04-15 21:09:29 -0500

    Resolved conflict by deleting and commenting out files.

--- models/index.js
+++ models/index.js
@@ -1,39 +1,39 @@
-// import models
-const User = require('./User');
-const Nerd_Type = require('./Nerd_Type');
-const Chatroom = require('./Chatroom');
-const Liked = require('./Liked');
+// // import models
+// const User = require('./User');
+// const Nerd_Type = require('./Nerd_Type');
+// const Chatroom = require('./Chatroom');
+// const Liked = require('./Liked');
 
 
-User.hasMany(Nerd_Type, {
-    foreignKey: 'user_id',
-    onDelete:"CASCADE"
-  });
+// User.hasMany(Nerd_Type, {
+//     foreignKey: 'user_id',
+//     onDelete:"CASCADE"
+//   });
   
-User.hasMany(Chatroom, {
-    foreignKey: 'user_id',
-    onDelete:"CASCADE"
-  });
+// User.hasMany(Chatroom, {
+//     foreignKey: 'user_id',
+//     onDelete:"CASCADE"
+//   });
   
-Nerd_Type.belongsTo(User, {
-    foreignKey: 'nerd_type',
-    onDelete:"CASCADE"
-  })
-Chatroom.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete:"CASCADE"
+// Nerd_Type.belongsTo(User, {
+//     foreignKey: 'nerd_type',
+//     onDelete:"CASCADE"
+//   })
+// Chatroom.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete:"CASCADE"
     
-  });
-Liked.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete:"CASCADE"
+//   });
+// Liked.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete:"CASCADE"
     
-  });
+//   });
 
 
-module.exports = {
-  User,
-  Nerd_Type,
-  Chatroom,
-  Liked
-};
+// module.exports = {
+//   User,
+//   Nerd_Type,
+//   Chatroom,
+//   Liked
+// };
