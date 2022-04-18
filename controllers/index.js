2192b65efe754eb91671f71a39c1a3ac33a21cd4
Anne Saber <iarabejo3@gmail.com>
2022-04-17 22:05:26 -0500

    Routes.

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,5 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
 module.exports = router;
 
