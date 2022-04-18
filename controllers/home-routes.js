134be009e76f40270b5fc2abc6d340b3bbc2ca7d
Vidalatan <vidalatan@gmail.com>
2022-04-18 00:36:22 -0500

    Connected pages
    Connected all pages via navbar and successful rendering in the routes.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,8 +17,30 @@ router.get("/homepage", withAuth, (req, res) => {
     res.status(500).json(err);
   }
 });
-// router.get('/dashboard')
-// router.get('/chatroom')
+
+router.get('/profile', withAuth, (req, res) => {
+  try {
+    res.render('profile')
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get('/dashboard', withAuth, (req, res) => {
+  try {
+    res.render('dashboard')
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+// router.get('/chatroom', withAuth, (req, res) => {
+//   try {
+    
+//   } catch (err) {
+    
+//   }
+// })
 // router.get('/chatroom/:id')
 
 router.get("/login", (req, res) => {
